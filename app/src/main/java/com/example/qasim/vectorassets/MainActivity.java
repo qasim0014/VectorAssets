d688bc58ccfc5c8aa3ab6cb782f24c48b1e3881c
Muhammad Qasim <qasim14@outlook.com>
2018-12-19 22:55:13 +0500

    Initial commit

---
+++ app/src/main/java/com/example/qasim/vectorassets/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.qasim.vectorassets;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView ivPic;
+    Button btnChange;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ivPic = findViewById(R.id.ivPic);
+        btnChange = findViewById(R.id.btnChange);
+
+        btnChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ivPic.setImageResource(R.drawable.box);
+            }
+        });
+
+    }
+}
